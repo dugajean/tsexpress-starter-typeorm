ba3d8a840bb50edf507f720d5b80e91499bdb606
Dugi <me@dukagj.in>
2019-08-20 20:51:01 +0200

    Updates dist

--- dist/connect.d.ts
+++ dist/connect.d.ts
@@ -2,6 +2,6 @@ import { Application } from '@tsexpress-starter/application';
 import { EntitySubscriberInterface } from 'typeorm';
 import insertStatus from './insert-status';
 import entityValidation from './entity-validation';
-export default function connect(app: Application, ...subscribers: EntitySubscriberInterface[]): Promise<void>;
+export declare function connect(app: Application, ...subscribers: EntitySubscriberInterface[]): Promise<void>;
 export declare const InsertStatus: typeof insertStatus;
 export declare const EntityValidation: typeof entityValidation;
